34f10b70017d9da94a6afc8382fa5cfa44335481
Juan Tapia <juanatg96@gmail.com>
2017-11-07 16:14:11 -0400

    Departamentos ApIs

---
+++ ActivosFijosAPI/Controllers/DepartamentoController.cs
@@ -0,0 +1,55 @@
+﻿using ActivosFijosAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ActivosFijosAPI.Controllers
+{
+    public class DepartamentoController : ApiController
+    {
+        // GET: api/Departamento
+        public IHttpActionResult Get()
+        {
+            var list = new Departamento().SelectDepartamentos();
+
+            return Ok(new { departamentos = list });
+        }
+
+        // GET: api/Departamento/5
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                Departamento departamento = new Departamento().SelectDepartamento(id);
+
+                return Ok(new { departamento });
+            }
+            catch (Exception e)
+            {
+                return Ok( new Departamento() );
+            }
+        }
+
+        // POST: api/Departamento
+        public void Post([FromBody]Departamento value)
+        {
+            Departamento.InsertDepartamento(value.Descripcion, value.Estado);
+        }
+
+        // PUT: api/Departamento/5
+        public void Put([FromBody]Departamento value)
+        {
+            Departamento.UpdateDepartamento(value.Id, value.Descripcion, value.Estado);
+        }
+
+        // DELETE: api/Departamento/5
+        public void Delete(int id)
+        {
+            Departamento.DeleteDepartamento(id);
+        }
+    }
+}
---
+++ ActivosFijosAPI/Models/Departamento.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActivosFijosAPI.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+
+        public List<Departamento> SelectDepartamentos()
+        {
+            Data.dsDepartamentosTableAdapters.DepartamentosTableAdapter adapter = new Data.dsDepartamentosTableAdapters.DepartamentosTableAdapter();
+            Data.dsDepartamentos.DepartamentosDataTable dt = adapter.SelectDepartamentos();
+
+            if (dt.Rows.Count <= 0)
+                return new List<Departamento>();
+
+            List<Departamento> listaDepartamentos = new List<Departamento>();
+
+            foreach (Data.dsDepartamentos.DepartamentosRow data in dt)
+            {
+                var departamento = new Departamento();
+
+                departamento.Id = data.ID_Departamento;
+                departamento.Descripcion = data.Descripcion;
+                departamento.Estado = data.Estado;
+
+                listaDepartamentos.Add(departamento);
+            }
+
+            return listaDepartamentos;
+        }
+
+        public Departamento SelectDepartamento(int idDepartamento)
+        {
+            Data.dsDepartamentosTableAdapters.DepartamentoTableAdapter adapter = new Data.dsDepartamentosTableAdapters.DepartamentoTableAdapter();
+            Data.dsDepartamentos.DepartamentoDataTable dt = adapter.SelectDepartamento(idDepartamento);
+
+            if (dt.Rows.Count <= 0)
+                return new Departamento();
+
+            this.Id = dt.First().ID_Departamento;
+            this.Descripcion = dt.First().Descripcion;
+            this.Estado = dt.First().Estado;
+
+            return this;
+        }
+
+        public static void InsertDepartamento(string descripcion, bool estado)
+        {
+            Data.dsDepartamentosTableAdapters.DepartamentoTableAdapter adapter = new Data.dsDepartamentosTableAdapters.DepartamentoTableAdapter();
+            adapter.InsertDepartamento(descripcion, estado);
+        }
+
+        public static void UpdateDepartamento(int idDepartamento, string descripcion, bool estado)
+        {
+            Data.dsDepartamentosTableAdapters.DepartamentoTableAdapter adapter = new Data.dsDepartamentosTableAdapters.DepartamentoTableAdapter();
+            adapter.UpdateDepartamento(idDepartamento, descripcion, estado);
+        }
+
+        public static void DeleteDepartamento(int idDepartamento)
+        {
+            Data.dsDepartamentosTableAdapters.DepartamentoTableAdapter adapter = new Data.dsDepartamentosTableAdapters.DepartamentoTableAdapter();
+            adapter.DeleteDepartamento(idDepartamento);
+        }
+    }
+}
